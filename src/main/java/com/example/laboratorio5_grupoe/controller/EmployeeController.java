438eb5c4989b27d6db5126dbe41745b1a70f265a
Angel Alvaro Bautista Cutipa <a20196044@pucp.edu.pe>
2022-10-04 21:05:09 -0500

    primera y segunda...

--- src/main/java/com/example/laboratorio5_grupoe/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio5_grupoe/controller/EmployeeController.java
@@ -1,28 +1,40 @@
 package com.example.laboratorio5_grupoe.controller;
 
+import com.example.laboratorio5_grupoe.entity.Employee;
+import com.example.laboratorio5_grupoe.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
+@RequestMapping(value = {"/empleado"})
 public class EmployeeController {
 
 
-    @GetMapping({"empleado/lista", "empleado"})
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping({"lista", ""})
     public String listEmployee(Model model, @RequestParam(name = "search",required = false) String search, @RequestParam(name = "order", required = false) Integer order, RedirectAttributes attributes){
 
 
-        return "XXXXXX";
+        model.addAttribute("listaEmpleados",employeeRepository.findAll());
+        return "employee/list";
     }
 
 
     //Buscar Empleado
-    public String searchEmployee(Model model, @RequestParam(name = "search",required = false) String search, @RequestParam(name = "order", required = false) Integer order, RedirectAttributes attributes){
+    @PostMapping({"search"})
+    public String searchEmployee(Model model, @RequestParam(name = "searchField",required = false) String searchField, @RequestParam(name = "order", required = false) Integer order, RedirectAttributes attributes){
 
-        return "XXXXXX";
+        List<Employee> listaEmployees=employeeRepository.buscarEmpleado(searchField);
+        model.addAttribute("listaEmpleados",listaEmployees);
+
+        return "employee/list";
     }
 
 
--- src/main/java/com/example/laboratorio5_grupoe/repository/EmployeeRepository.java
+++ src/main/java/com/example/laboratorio5_grupoe/repository/EmployeeRepository.java
@@ -1,8 +1,31 @@
 package com.example.laboratorio5_grupoe.repository;
 
+import com.example.laboratorio5_grupoe.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 //Completar
 @Repository
-public interface EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+
+
+
+    @Transactional
+    @Query(value = """
+            select * from employees e
+            inner join jobs j
+            on (e.job_id=j.job_id)
+            inner join departments dep
+            on (e.department_id=dep.department_id)
+            inner join locations loc
+            on (dep.location_id=loc.location_id)
+            where LOWER(first_name) like LOWER(concat('%',?1,'%')) OR LOWER(last_name) like LOWER(concat('%',?1,'%')) OR LOWER(j.job_title) LIKE LOWER (concat('%',?1,'%')) OR LOWER(dep.department_name) LIKE LOWER(concat('%',?1,'%')) OR LOWER(loc.city) like LOWER(concat('%',?1,'%'))\s""",nativeQuery = true)
+    List<Employee> buscarEmpleado(String searchField);
+
+
 }
