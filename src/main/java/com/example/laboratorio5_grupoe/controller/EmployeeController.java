57a46f85a63a196f0b999307e042dba195c64bad
Angel Alvaro Bautista Cutipa <a20196044@pucp.edu.pe>
2022-10-04 21:58:17 -0500

    semifcmit

--- src/main/java/com/example/laboratorio5_grupoe/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio5_grupoe/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package com.example.laboratorio5_grupoe.controller;
 import com.example.laboratorio5_grupoe.entity.Employee;
 import com.example.laboratorio5_grupoe.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.Objects;
 import java.util.Optional;
 
 import java.util.List;
@@ -27,6 +29,22 @@ public class EmployeeController {
 
 
         model.addAttribute("listaEmpleados",employeeRepository.findAll());
+        return "employee/list";
+    }
+
+    @PostMapping({"sort"})
+    public String sortEmployee(Model model,@RequestParam(name = "sorted",required = false) String sorted,RedirectAttributes attr) {
+
+        if (sorted == null){
+            model.addAttribute("listaEmpleados",employeeRepository.findAll(Sort.by(Sort.Order.asc("firstName"))));
+            attr.addFlashAttribute("sorted","xd");
+        }
+
+
+         if (Objects.equals(sorted, "xd")){
+            model.addAttribute("listaEmpleados",employeeRepository.findAll(Sort.by(Sort.Order.desc("firstName"))));
+        }
+
         return "employee/list";
     }
 
--- src/main/java/com/example/laboratorio5_grupoe/repository/EmployeeRepository.java
+++ src/main/java/com/example/laboratorio5_grupoe/repository/EmployeeRepository.java
@@ -12,7 +12,6 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
-/**/
 
 
     @Transactional
