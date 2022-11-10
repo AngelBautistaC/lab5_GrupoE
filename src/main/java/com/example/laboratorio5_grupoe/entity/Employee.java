a35f0eb28532f61dbc7c8e1c9e1123e3132a7459
rafoar4 <a20180217@pucp.edu.pe>
2022-11-10 00:17:22 -0500

    login creo q ahora si final

--- src/main/java/com/example/laboratorio5_grupoe/entity/Employee.java
+++ src/main/java/com/example/laboratorio5_grupoe/entity/Employee.java
@@ -1,10 +1,7 @@
 package com.example.laboratorio5_grupoe.entity;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.io.Serializable;
 import java.time.Instant;
